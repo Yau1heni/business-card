6e9c84aa3eccadecee2e2224795a09c572be776d
Yau1heni <lodbrok9@gmail.com>
2023-10-03 21:01:24 +0300

    first commit

---
+++ tests/BusinessCard.spec.ts
@@ -0,0 +1,40 @@
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { toNano } from 'ton-core';
+import { BusinessCard } from '../wrappers/BusinessCard';
+import '@ton-community/test-utils';
+
+describe('BusinessCard', () => {
+    let blockchain: Blockchain;
+    let businessCard: SandboxContract<BusinessCard>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        businessCard = blockchain.openContract(await BusinessCard.fromInit());
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await businessCard.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: businessCard.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and businessCard are ready to use
+    });
+});
