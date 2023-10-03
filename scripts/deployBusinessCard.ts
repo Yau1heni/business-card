8057c7b2181ed01bfe47c51f5d74e36773b56d1c
Yau1heni <lodbrok9@gmail.com>
2023-10-03 23:15:37 +0300

    add contract business card

--- scripts/deployBusinessCard.ts
+++ scripts/deployBusinessCard.ts
@@ -1,22 +1,30 @@
 import { toNano } from 'ton-core';
-import { BusinessCard } from '../wrappers/BusinessCard';
+import { BusinessCard, User } from '../wrappers/BusinessCard';
 import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const businessCard = provider.open(await BusinessCard.fromInit());
 
-    await businessCard.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'Deploy',
-            queryId: 0n,
-        }
-    );
+  const userInfo: User = {
+    $$type: 'User',
+    name: "Liasun Yauheni",
+    profession: "Frontend Developer",
+    bio: "I'll add more later:)"
+  }
 
-    await provider.waitForDeploy(businessCard.address);
+  const businessCard = provider.open(await BusinessCard.fromInit(userInfo));
 
-    // run methods on `businessCard`
+  await businessCard.send(
+    provider.sender(),
+    {
+      value: toNano('0.05'),
+    },
+    {
+      $$type: 'Deploy',
+      queryId: 0n,
+    }
+  );
+
+  await provider.waitForDeploy(businessCard.address);
+
+  // run methods on `businessCard`
 }
